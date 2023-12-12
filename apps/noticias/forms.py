13438417d5aed3dbec9f32dc47437c9961db6047
pablopassero <ivan_pass01@homtail.com>
2023-12-12 09:36:59 -0300

    modificar y borrar noticia de nuevo

--- apps/noticias/forms.py
+++ apps/noticias/forms.py
@@ -6,7 +6,6 @@ class Formulario_Noticia(forms.ModelForm):
 
 	class Meta:
 		model = Noticia
-<<<<<<< HEAD
 		fields = ['titulo','contenido','imagen','categoria']
 
 class Formulario_Modificar_Noticia(forms.ModelForm):
@@ -14,6 +13,3 @@ class Formulario_Modificar_Noticia(forms.ModelForm):
 	class Meta:
 		model = Noticia
 		fields = ['contenido','imagen','categoria']		
-=======
-		fields = ['titulo','contenido','imagen','categoria']
->>>>>>> 44153ea4e5a98109987d83981a0304487e11cdc3
